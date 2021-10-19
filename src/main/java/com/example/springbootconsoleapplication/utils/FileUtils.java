b7ba774b090eebf99deb7a7b30d585d16d5213e3
exceq <mihail.petuxov@yandex.ru>
2021-10-20 01:24:15 +0500

    Очепятка

--- src/main/java/com/example/springbootconsoleapplication/utils/FileUtils.java
+++ src/main/java/com/example/springbootconsoleapplication/utils/FileUtils.java
@@ -2,7 +2,7 @@ package com.example.springbootconsoleapplication.utils;
 
 public class FileUtils {
     public static String getFileExtension(String filename) {
-        if (filename.contains("."))
+        if (!filename.contains("."))
             return "";
         return filename.substring(filename.lastIndexOf('.') + 1);
     }
